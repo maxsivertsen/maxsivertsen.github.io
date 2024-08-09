7f7d5dab31eb2eba671e246acd048b7b5ba43aae
mg297bgyy <mg297bgyy@uni-leipzig.de>
2024-08-09 11:34:11 +0200

    update photos

---
+++ scripts/nature_photo_file_names.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+
+site_dir = '/home/empathyforgiveness/max_website/max-website/'
+
+# Directory containing images
+image_dir = site_dir + 'assets/photography/nature'
+
+# List to store image filenames
+photos = []
+
+# Loop through files in the directory
+for filename in os.listdir(image_dir):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):
+        photos.append(filename)
+
+# Write to photos.yml
+with open(site_dir + '_data/photos.yml', 'w') as file:
+    yaml.dump(photos, file, default_flow_style=False)
